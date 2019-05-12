df96566178091ea8129432b16ca9b19b53d448d0
Kamrul_Hasan <48267671+kicchuparinah@users.noreply.github.com>
2019-05-13 01:14:45 +0600

    Add files via upload

---
+++ relatively prime_Co_Prime_Euler_phi(efficient).cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include<stdio.h>
+using namespace std;
+///...................................*****.................................................///
+///                  Kamrul Hasan ( icthasan36@gmail.com )                                  ///
+///                  Department of Ict                                                      ///
+///                  Comilla University , Bangladesh.                                       ///
+///...................................*****.................................................///
+#define           ll               long long int
+
+ll phi(ll a)
+{
+    ll res=a;
+    for(ll i=2; i*i<=a;i++){
+        if(a%i==0){
+            while(a%i==0){
+                a=a/i;
+            }
+            res=res-(res/i);
+        }
+    }
+    if(a>1){
+        res=res-(res/a);
+    }
+    return res;
+}
+
+
+int main()
+{
+    ll a;
+    while(1)
+    {
+        scanf("%lld",&a);
+        if(a==0){
+            break;
+        }
+        printf("%lld\n",phi(a));
+    }
+}
