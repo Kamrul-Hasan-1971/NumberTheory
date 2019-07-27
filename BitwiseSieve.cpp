79376f9b7556dbbe788fb1ab2da0f2324aeb2563
Kamrul_Hasan <48267671+Sara-Hasan@users.noreply.github.com>
2019-07-27 16:38:37 +0600

    Add files via upload

---
+++ BitwiseSieve.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector< ll > v ;
+const int MAXN = 1e7 ;
+bitset< MAXN > status ;
+
+void findPrime()
+{
+    int rt = sqrt( MAXN ) + 1 ;
+
+    for( int i = 3 ; i < rt ; i += 2 )
+    {
+        if( !status[ i ] )
+        {
+            for( int j = i * i ; j < MAXN ; j += ( i << 1 ) )
+            {
+                 status[j] = true;
+            }
+        }
+    }
+
+    v.push_back( 2 ) ;
+
+    for( int i = 3; i < MAXN; i += 2 )
+    {
+        if( !status[ i ] )
+        {
+            v.push_back( i ) ;
+        }
+    }
+}
