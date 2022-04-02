ec4feb1543b617d6184e36dd2cf6946e67b175e0
Kamrul_Hasan <48267671+Kamrul-Hasan-1971@users.noreply.github.com>
2022-04-02 12:43:46 +0600

    Update factorial factorization.cpp

--- factorial factorization.cpp
+++ factorial factorization.cpp
@@ -1,55 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
-vector<long long int>v1;
 #define pii pair<long long int,long long int>
 #define ll long long int
-vector<pii>v;
 
+vector<long long int>prime;
+vector<pii>v;
 
 bool status[115];
 void siveve(int N)
 {
-    int sq=sqrt(N);
-    for(int i=4; i<=N; i+=2)
-    {
-        status[i]=1;
-    }
-    for(int i=3; i<=sq; i+=2)
-    {
-        if(status[i]==0)
-        {
-            for(int j=i*i; j<=N; j+=i)
-            {
+    for(int i=4; i<=N; i+=2) status[i]=1;
+    for(int i=3; i*i <= N; i+=2)
+        if(!status[i])
+            for(int j=i*i; j <= N; j += (i*2))
                 status[j]=1;
-            }
-        }
-    }
     status[1]=1;
     status[0]=1;
-    for(int i=1; i<=N ; i++)
-    {
-        if(status[i]==0)
-        {
-            v1.push_back(i);
-        }
-    }
-
+    prime.push_back(2);
+    for(int i=3; i<=N ; i+=2)
+        if(!status[i])
+            prime.push_back(i);
 }
 
 void factFactorization(int r)
 {
-    int num=0;
-    for(int i=0;i<v1.size();i++){
-        int s=r;
+    for(int i=0;i<prime.size();i++){
+        int s=r,num=0;
         while(1){
-            s=s/v1[i];
-            num=num+s;
-            if(s==0){
-                break;
-            }
+            s /= prime[i];
+            num += s;
+            if(s==0) break;
         }
-        v.push_back(make_pair(v1[i],num));
-        num=0;
+        v.push_back({prime[i],num});
     }
 }
 
@@ -66,7 +48,7 @@ int main()
         for(j=1;j<v.size();j++){
             printf(" * %lld (%lld)",v[j].first,v[j].second);
         }
-        v1.clear();
+        prime.clear();
         v.clear();
         printf("\n");
     }
